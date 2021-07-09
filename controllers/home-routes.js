0f1e0ce12d768184904597be414f28c90618e65f
siobhan <sr@saibhreas.com>
2021-07-09 07:45:44 -0400

    changed routes to add express const express = require ('express'); const router = express.Router();

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,6 @@
-const router = require('express').Router();
+
+const express = require ('express');
+const router = express.Router();
 const { Post, Comment, User } = require('../models/');
 
 // get all posts for homepage
